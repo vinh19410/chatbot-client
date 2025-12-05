9da97965b7d1fc0f13e0e8b70e256b7340246684
Vinh Tran <33002590+vinh19410@users.noreply.github.com>
2025-12-05 22:08:50 +0700

    fix node version

--- src/plugins/iconify/build-icons.ts
+++ src/plugins/iconify/build-icons.ts
@@ -12,11 +12,17 @@
  */
 import { promises as fs } from 'node:fs'
 import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { createRequire } from 'node:module'
 
 // Installation: npm install --save-dev @iconify/tools @iconify/utils @iconify/json @iconify/iconify
 import { cleanupSVG, importDirectory, isEmptyColor, parseColors, runSVGO } from '@iconify/tools'
 import type { IconifyJSON } from '@iconify/types'
 import { getIcons, getIconsCSS, stringToIcon } from '@iconify/utils'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+const require = createRequire(import.meta.url)
 
 /**
  * Script configuration
